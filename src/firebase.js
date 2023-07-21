04061cf0a4466ab284f72c1e175ca73e7a593c36
Vishal K <116670999+Vishalk91-4@users.noreply.github.com>
2023-07-21 21:31:33 +0530

    Updated firebase.js with new config

--- src/firebase.js
+++ src/firebase.js
@@ -2,14 +2,14 @@ import firebase from "firebase/compat/app";
 import "firebase/compat/database";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBIYjI17NHZO-XWq0kmqOXlSAZoUnBpywg",
-    authDomain: "tech-team-48ffd.firebaseapp.com",
-    databaseURL: "https://tech-team-48ffd-default-rtdb.firebaseio.com",
-    projectId: "tech-team-48ffd",
-    storageBucket: "tech-team-48ffd.appspot.com",
-    messagingSenderId: "342250297681",
-    appId: "1:342250297681:web:52f6e788fa2551f3dcdb70"
-  };
+  apiKey: "AIzaSyA0hA2Bc74PEzeuoahNApERIEkxFqv_jSo",
+  authDomain: "techteamexpansion23.firebaseapp.com",
+  projectId: "techteamexpansion23",
+  storageBucket: "techteamexpansion23.appspot.com",
+  messagingSenderId: "542718392377",
+  appId: "1:542718392377:web:bf6178e1b28494fe8c35e8",
+  measurementId: "G-39B1R2EWV2"
+};
 
 const fireDb = firebase.initializeApp(firebaseConfig);
 
